12241f0d1b536caf608c2ac1c7eeac9851ee7551
Silvia Trejo <s.y.trejo26@gmail.com>
2021-12-08 16:45:13 -0500

    Updating files...The color coding is not working yet.

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,42 @@
-var dt = moment(myDate.date, "YYYY-MM-DD HH:mm:ss")
-dt.format('dddd');
+// //This will display the date and time at the top.
+moment(Date);
+$("#currentDay").text(moment().format('dddd MMMM Do YYYY, h:mm a'));
+var currentTime = moment();
+currentTime = currentTime.startOf('hour');
+var beforeTime = moment().startOf('day').add(9, 'hours');
+
+// // This will save to the local storage
+function saveData(){
+    localStorage.saveServer
+    console.log('This happened')
+}
+
+//to change the color of each block...I can't figure out what I am doing wrong...
+
+// $(init);
+
+// function init(){
+//     $("#currentDay").text(moment().format("dddd, MMMM Do"));
+//     colorTimeblocks();
+//     setInterval(colorTimeblocks, 60000);
+//     $(".time-block").each(function(){
+//         var blockId = $(this).attr("id");
+//         $("#" + blockId + "textarea").text(localStorage.getItem(moment().format("DDDYYYY")+ blockId));
+//         $(".saveBtn").on("click", handleSave);
+//     })
+// }
+
+// function colorTimeblocks(){
+//     $('.time-block').each(function(){
+//         var blockHour = parseInt($(thi).attr("id").replace("hour",""));
+//         var currentHour = parseInt(moment().format("H"));
+//         $(this).removeClass("past present future");
+//         if(blockHour < currentHour){
+//             $(this).addClass("past");
+//         } else if (blockHour > currentHour){
+//             $(this).addClass("future");
+//         } esle {
+//             $(this).addClass("present");
+//         }
+//     })
+// }
